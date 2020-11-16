b6aac2ccd67a74a6d98c14b8c87cee5dfc2332b9
Nicolas <nicolas.berti69@gmail.com>
2020-11-16 11:42:39 -0300

    Programacion:: Base

--- src/logica/Juego.java
+++ src/logica/Juego.java
@@ -2,4 +2,44 @@ package logica;
 
 public class Juego {
 
+	private int nivelActual;
+	private Mapa miMapa; // La clase encargada de dar las ImageIcon es la clase "EntidadGrafica" con el metodo getGrafico();
+	
+	/**
+	 * Creación del juego.
+	 * @param niveles Cantidad de niveles.
+	 */
+	public Juego(int niveles) {
+		// tamaño del mapa
+		int x = 200;
+		int y = 200;
+		miMapa = new Mapa(x,y,niveles);
+		nivelActual = 0;
+	}
+	
+	public void empezar() {
+		nivelActual = 1; // Los niveles actuales en el backend es nivelActual-1 y en el frontend simplemente es nivelActual
+	}
+	
+	public void pasarNivel() {
+		
+	}
+	
+	public void ganar() {
+		
+	}
+	
+	public void perder() {
+		
+	}
+	
+	public Mapa getMapa() {
+		return miMapa;
+	}
+	
+	public int getNivelActual() {
+		return nivelActual;
+	}
+
+	
 }
--- src/logica/Mapa.java
+++ src/logica/Mapa.java
@@ -2,4 +2,7 @@ package logica;
 
 public class Mapa {
 
+	public Mapa(int x, int y, int niveles) {
+		
+	}
 }

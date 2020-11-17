f2a1f7f0a1f4bebd28d482045cf0f9492b95dcee
Nicolas <nicolas.berti69@gmail.com>
2020-11-17 13:47:06 -0300

    Programacion:: Entidades base

--- src/entidades/Entidad.java
+++ src/entidades/Entidad.java
@@ -1,5 +1,12 @@
 package entidades;
 
+import logica.*;
+
 public abstract class Entidad {
 
+	protected int velocidad;
+	protected Posicion posicion;
+	
+	public abstract void accept(Visitor visit);
+	
 }
--- src/entidades/Jugador.java
+++ src/entidades/Jugador.java
@@ -1,5 +1,38 @@
 package entidades;
 
+import logica.*;
+
 public class Jugador extends Entidad {
 
+	private int cargaViral;
+	
+	public Jugador(int velocidad) {
+		super.posicion = new Posicion();
+		super.velocidad = velocidad;
+	}
+	
+	public void disparar() {
+		
+	}
+	
+	public void usarEfecto(int indice) {
+		
+	}
+	
+	public void moverDerecha() {
+		
+	}
+	
+	public void moverIzquierda() {
+		
+	}
+	
+	public void usarPremio(Premio p) {
+		
+	}
+	
+	public void accept(Visitor visit) {
+		
+	}
+		
 }
